6134e1033049e42b04b4c27f77861fef5a4f7949
Ben Pfaff <blp@cs.stanford.edu>
2020-06-25 22:14:12 -0700

    Eliminate em dashes from chapter number delimiters.

--- toc.py
+++ toc.py
@@ -13,6 +13,22 @@ def ab_ns(tag):
 import sys
 output_file = sys.argv[1]
 
+def chapter_title_text(element):
+    tag = element.tag
+    if not isinstance(tag, str) and tag is not None:
+        return
+
+    t = element.text
+    if t:
+        yield t
+        if tag == html_ns("div"):
+            yield ": "
+    for e in element:
+        yield from chapter_title_text(e)
+        t = e.tail
+        if t:
+            yield t
+
 nav_map = ET.Element('navMap')
 chapter = 0
 for h2 in ET.parse('twir.xhtml').findall('./html:body/html:h2', ns):
@@ -20,7 +36,8 @@ for h2 in ET.parse('twir.xhtml').findall('./html:body/html:h2', ns):
     nav_point = ET.SubElement(nav_map, 'navPoint', {'playOrder': '%d' % chapter})
     nav_label = ET.SubElement(nav_point, 'navLabel')
     text = ET.SubElement(nav_label, 'text')
-    text.text = re.sub('\s+', ' ', ''.join(h2.itertext())).rstrip('*')
+    title = ''.join(chapter_title_text(h2))
+    text.text = re.sub('\s+', ' ', title.rstrip('*'))
     ET.SubElement(nav_point, 'content', {'src': 'twir_%d.xhtml' % chapter})
 
 ET.register_namespace('', 'http://www.daisy.org/z3986/2005/ncx/')
